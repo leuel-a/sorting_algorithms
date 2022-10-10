1a3505c9ac762570090b7b562f0782cf1c918dbb
nefelibatala1993 <leuela1993@gmail.com>
2022-10-10 20:36:07 +0300

    Added 2-O and updated 1-insertion_sort.c

--- 1-insertion_sort.c
+++ 1-insertion_sort.c
@@ -1,36 +1,44 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - this function sorts a doubly linked list
+ * based on the Insertion Sort Algorithm.
+ * @list: the list to be sorted
+ *
+ * Return: Nothing
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *aux, *temp, *next;
 
-	curr = (*list)->next;
+	if (!list)
+		return;
 
+	curr = *list;
 	while (curr != NULL)
 	{
 		next = curr->next;
-
-		aux = *list;
-		while (aux->next && aux->n > curr->n)
+		aux = curr->prev;
+		while (aux != NULL && aux->n > curr->n)
 		{
+			temp = curr->next;
+			aux->next = temp;
+
+			if (temp != NULL)
+				temp->prev = aux;
+
 			temp = aux->prev;
-			aux->next = curr->next;
-			(curr->next)->prev = aux;
 			aux->prev = curr;
 			curr->next = aux;
-			
+			curr->prev = temp;
+
 			if (temp == NULL)
-			{
-				curr->prev = NULL;
 				*list = curr;
-			} else
-			{
+			else
 				temp->next = curr;
-				curr->prev = temp;
-			}
+			aux = temp;
+			print_list(*list);
 		}
-		
-		print_list(*list);
 		curr = next;
 	}
 }
